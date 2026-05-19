f087c28df1c35f32f68844fef3eaaece7b9fcf36
Lucian <90592237+lungDa@users.noreply.github.com>
2026-05-19 19:21:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,16 @@ col_left, col_center, col_right = st.columns([1, 3, 1.5], gap="large")
 with col_left: 
     st.write("###  新增區") 
     st.button("按鈕放左邊")
+    with st.container(border=True): 
+    st.write(" 標題：開學典禮") 
+    st.write(" 時間：09:00")
 
 with col_center: 
     st.write("###  看板區") 
     st.info("主要行程訊息放中間")
+    with st.container(border=True): 
+    st.write(" 標題：開學典禮") 
+    st.write(" 時間：09:00")
 
 with col_right: 
     st.write("###  設定區") 
